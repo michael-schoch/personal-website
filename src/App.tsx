95fbe7b254d3a4e284f311f3e199540002865737
Mike Schoch <michael.schoch@aviture.us.com>
2023-05-24 14:44:46 -0500

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import './App.css';
+import styled from 'styled-components'
+import portrait from './portrait.png'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContainer>
+      <Header>
+        <Link href='#'>About Mike</Link>
+        <Link href='#'>Resume</Link>
+        <Portrait src={portrait}/>
+        <Link href='#'>Portfolio</Link>
+        <Link href='#'>Contact</Link>
+      </Header>
+      <Title>Michael Schoch</Title>
+      <Subheader>Software Enginer / User Experience</Subheader>
+
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+ height: 100vh;
+ background-color: #241E4E;
+ display: flex;
+ width: 100%;
+ justify-content: center;
+ flex-direction: column;
+ align-items: center;
+`
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+  align-items: center;
+`
+
+const Portrait = styled.img`
+  width: 200px;
+  height: 200px;
+`
+
+const Title = styled.h1`
+  display: flex;
+  color: white;
+`
+
+const Subheader = styled.h2`
+  display: flex;
+  color: white;
+  margin: 0
+`
+
+const Link = styled.a`
+  color: white;
+  text-decoration: none;
+`
 
 export default App;
