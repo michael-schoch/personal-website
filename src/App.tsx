52da03a5b61d1b8fe6037e7304389ce4eefade97
Mike Schoch <michael.schoch@aviture.us.com>
2023-05-28 22:13:56 -0500

    removed mobile height from apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,19 @@
 import styled from "styled-components";
 import { Menu } from "./header/menu";
 import { BrowserRouter as Router } from "react-router-dom";
-import { handleMobileHeight } from "./helpers/handle-mobile-height";
-
-interface AppConProps {
-  $height: string | null;
-}
 
 function App() {
   return (
     <Router>
-      <AppContainer $height={handleMobileHeight()}>
-        <Float>I AM A</Float>
+      <AppContainer>
         <Menu />
       </AppContainer>
     </Router>
   );
 }
 
-const Float = styled.div`
-  position: absolute;
-  top: 55vh;
-  left: 45vw;
-  width: 200px;
-  color: white;
-  font-size: 36px;
-  z-index: 1;
-`;
-
-const AppContainer = styled.div<AppConProps>`
-  height: ${(props) => (props.$height !== null ? props.$height : "100vh")};
+const AppContainer = styled.div`
+  height: 100vh;
   display: flex;
   width: 100%;
   justify-content: center;
